f54c53b6d412ab5d10eee51d55ed4728f28c37b5
Kiruthikaa P <p.kiruthikaa@gmail.com>
2019-06-18 20:43:18 +0530

    second commit

--- src/testGit/testClass.java
+++ src/testGit/testClass.java
@@ -5,6 +5,11 @@ public class testClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("hello world");
+      int i = 7;
+      if(i > 20)
+    	  System.out.println("greater than 20");
+      else
+    	  System.out.println("lesser than 20");
 	}
 
 }
