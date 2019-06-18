90246d5988b3b00c3040a6dda350949663fa54b1
Kiruthikaa P <p.kiruthikaa@gmail.com>
2019-06-18 23:11:44 +0530

    poc main commit

--- src/testGit/testClass.java
+++ src/testGit/testClass.java
@@ -3,8 +3,8 @@ package testGit;
 public class testClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
       System.out.println("hello world");
+      //demo pgm
       int i = 7;
       System.out.println("checking number greater than 20");
       //actual check
